c70c8414234b0a7e58545d793e1f4678b2ef0613
vovisim_home <wova53209@yandex.ru>
2026-02-05 01:32:00 +0300

    Настройка деплоя на gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import path from 'node:path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
+  base: './accounts-manager/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
